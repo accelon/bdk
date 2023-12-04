a6fbc11ad26a53bc44e4c8b367bf18124c7ebc6e
yap cheah shen <yapcheahshen@gmail.com>
2023-12-04 14:42:58 +0800

    chinese first split

---
+++ src/appstore.js
@@ -0,0 +1,19 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::bdk',
+_ACC23.AppVer='23.10.31';
+_ACC23.AppName='bdk'
+_ACC23.AppTitle='我的應用'
+_ACC23.allptks=['bdk','bdk-en',"bdk-ru"];
+_ACC23.selectedptks=['bdk','bdk-en']
+_ACC23.activeptk='bdk'
+_ACC23.ptkinfo={
+    'bdk':"中文版",
+    'bdk-en':"英文版",
+    'bdk-ru':"俄文版",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
